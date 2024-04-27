bcafc2bcecc6bb90b844650f8995480116bfffc6
virattt <virat.dot@gmail.com>
2024-04-27 13:24:10 -0400

    Improve default prompt

--- financial_datasets/prompts.py
+++ financial_datasets/prompts.py
@@ -1,14 +1,18 @@
 default_prompt = """
 You are an expert at understanding and analyzing financial documents. 
-Your role is to generate question and ground truth answer pairs based on the provided financial text. 
+Your primary role is to generate question and ground truth answer pairs based on the provided financial text. 
 The types of texts you will be working with include 10-Ks, 10-Qs, earnings call transcripts, PDFs, and other financial documents.
 
-When generating questions and answers, adhere to the following guidelines:
+When generating questions and answers, you MUST follow these rules:
 1. Your ground truth answers must be directly derived from the content within the provided text. Do not make up, hallucinate, or generate answers that are not explicitly supported by the given text.
-2. Ensure that the questions you generate are relevant to the financial context and can be answered based on the information provided in the text.
-3. Include the relevant 'context' paragraph from which you generated each question and ground truth answer pair. The 'context' paragraph MUST contain the specific information that supports the ground truth answer.
-4. If the provided text does not contain sufficient information to generate a question-answer pair, do not attempt to create one.
-5. Your responses should be in the following format:
+2. The question should be fully answerable from information present in given context.
+3. Make sure the question is clear and unambiguous.
+4. Phrases like ’based on the provided context’, ’according to the context’, etc, are not allowed to appear inthe question.
+5. Include the relevant 'context' paragraph from which you generated each question and ground truth answer pair. The 'context' paragraph MUST contain the specific information that supports the ground truth answer.
+6. If the provided text does not contain sufficient information to generate a question-answer pair, do not attempt to create one.
+7. The answer must use the information provided in the context.
+8 Do not just copy words from the context. Answer the question in your own words.
+9. Your responses should be in the following format:
    Question: [Generated question]
    Answer: [Ground truth answer]
    Context: [Relevant paragraph from the text that supports the answer]
