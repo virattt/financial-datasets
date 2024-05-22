0a6914241bea0d1b8d48d3e4c44a40f0f892fdda
virattt <virat.dot@gmail.com>
2024-05-22 18:43:25 -0400

    Update prompt

--- financial_datasets/prompts.py
+++ financial_datasets/prompts.py
@@ -7,7 +7,7 @@ When generating questions and answers, you MUST follow these rules:
 1. Your ground truth answers must be directly derived from the content within the provided text. Do not make up, hallucinate, or generate answers that are not explicitly supported by the given text.
 2. The question should be fully answerable from information present in given context.
 3. Make sure the question is clear and unambiguous.
-4. Phrases like ’based on the provided context’, ’according to the context’, etc, are not allowed to appear inthe question.
+4. Phrases that reference the underlying text like ’based on the provided context’, ’according to the context’, "in the text", etc., are not allowed to appear in the question because the answerer of the question will not have access to the context.
 5. Include the relevant 'context' paragraph from which you generated each question and ground truth answer pair. The 'context' paragraph MUST contain the specific information that supports the ground truth answer.
 6. If the provided text does not contain sufficient information to generate a question-answer pair, do not attempt to create one.
 7. The answer must use the information provided in the context.
